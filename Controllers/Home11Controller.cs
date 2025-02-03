8fdc121664f30907e6fc4155507c2433d0b2e10c
Hemida <adhamhemida4@gmail.com>
2025-02-03 15:58:48 +0200

    added homm11 controller

---
+++ Controllers/Home11Controller.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+public class Home11Controller : Controller
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
